1bc6b1a615700c28c49892591615a6d261c51f56
Helen Marshall <marshallhelenm@gmail.com>
2019-06-26 18:22:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,3 +7,13 @@ end
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+#introduction_with_language_optional
+def introduction_with_language_optional (name, language)
+  if langage = nil
+    puts "Hi, my name is #{name}."
+else
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+  
+end
